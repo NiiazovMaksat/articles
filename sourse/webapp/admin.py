970fd5ff7f112589f14dd4f9d61cb9e4b5c72c6b
NiiazovMaksat <niyazov37@gmail.com>
2022-01-02 20:14:27 +0600

    Article зарегистророван в Панели Администратора

--- sourse/webapp/admin.py
+++ sourse/webapp/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from webapp.models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'created_at']
+    list_filter = ['author']
+    search_fields = ['title', 'content']
+    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Article, ArticleAdmin)
