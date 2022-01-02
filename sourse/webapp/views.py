a8f3980f27f67dfc6cbdab09c6ea72ac718f6115
NiiazovMaksat <niyazov37@gmail.com>
2022-01-02 22:19:08 +0600

    Изменил пути для просмотра статьи, дал имена Юрлкам

--- sourse/webapp/views.py
+++ sourse/webapp/views.py
@@ -16,10 +16,10 @@ def create_article_view(request):
         author = request.POST.get('author')
         new_article = Article.objects.create(title=title, content=content, author=author)
 
-        return HttpResponseRedirect(f"/article/?pk={new_article.pk}")
+        return HttpResponseRedirect(f"/article/{new_article.pk}")
 
-def article_view(request):
-    pk = request.GET.get("pk")
+def article_view(request, pk):
+    #pk = request.GET.get("pk")
     article = Article.objects.get(pk=pk)
     context = {"article": article}
     return render(request, "article_view.html", context)
