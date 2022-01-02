e4549b38b2f2e944ec415c6fd8418ccb5e142bc9
NiiazovMaksat <niyazov37@gmail.com>
2022-01-02 19:54:05 +0600

    создана модель Артикл и создана мграция

--- sourse/webapp/models.py
+++ sourse/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Заголовок")
+    author = models.CharField(max_length=100, null=False, blank=False, verbose_name="Автор", default="Unknown")
+    content = models.TextField(max_length=2000, null=False, blank=False, verbose_name="Контент")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.pk}){self.author}: {self.title}"
+
+    class Meta:
+        db_table = "Articles"
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
